f4903daf360bd9ff6785ffb3c8782978dafa1144
rishabhkesarwani <rkesari2002@gmail.com>
2022-01-22 19:12:52 +0530

    Basic of java

--- java_Intro/operator.java
+++ java_Intro/operator.java
@@ -102,5 +102,7 @@ public class operator {
         result = (februaryDays == 28) ? "Not a leap year" : "Leap year";
         System.out.println(result);
 
+        System.out.println("End");
+
     }
 }
